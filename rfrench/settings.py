025cbffd35a68a48f571242431554c2093634332
Robert French <rfrench@rfrench.org>
2018-03-25 15:36:33 -0700

    Add ALLOWED_HOSTS'

--- rfrench/settings.py
+++ rfrench/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rfrench.org', 'robertfrench.org', 'robert-french.org']
 
 
 # Application definition
